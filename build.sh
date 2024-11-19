538fa3f62fbfd825ce043ef739d3dcae4ff86dbb
gagahsyuja <gghssa1@gmail.com>
2024-11-19 20:50:06 +0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo rm -rf .work
 sudo rm -rf out-baseline
-sudo mkarchiso -r -v -w .work -o out baseline
+sudo mkarchiso -r -v -w .work -o out tealinux
--- build_baseline_mnt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo rm -rf .work
-sudo mkarchiso -r -v -w .work -o /mnt baseline
