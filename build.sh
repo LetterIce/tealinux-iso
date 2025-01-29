9d97d8f2b945a613fb8b503156e99a28255a9bb1
Khoirul Anam (yourside) <akunnyakhoirulanam@gmail.com>
2025-01-30 03:38:13 +0700

    Use systemd-inhibit for mkarchiso command in build script; suppress mpv output

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo rm -rf .work
 start_time=$(date +%s)
-time sudo mkarchiso -r -v -w .work -o out tealinux
+time sudo systemd-inhibit mkarchiso -r -v -w .work -o out tealinux
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time))
 
 notify-send -u critical "Tealinux Build" "Exited. done in $elapsed_time seconds"
-mpv notif.mp3
+mpv notif.mp3 > /dev/null 2>&1 &
