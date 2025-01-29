2d1d60e3653a9e2baa03a600bd2d91bdf3eb0279
Khoirul Anam (yourside) <akunnyakhoirulanam@gmail.com>
2025-01-29 16:57:02 +0700

    Enhance build script with timing and notification features; update package selections for Plasma desktop environment

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sudo rm -rf .work
-sudo rm -rf out-baseline
-sudo mkarchiso -r -v -w .work -o out tealinux
+start_time=$(date +%s)
+time sudo mkarchiso -r -v -w .work -o out tealinux
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+notify-send -u critical "Tealinux Build" "Exited. done in $elapsed_time seconds"
+mpv notif.mp3
