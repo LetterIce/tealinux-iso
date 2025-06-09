5c0c671e41fd2c688dad3cde837f045b1778f653
gagahsyuja <gghssa1@gmail.com>
2025-06-10 02:38:38 +0700

    Improve build script

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sudo sed -i "s|Server = file://.*|Server = file://$(pwd)/localrepo/|" ./tealinux/pacman.conf
+
+start_time=$(date +%s)
+
+# Build ISO
 sudo rm -rf .work
-sudo mkarchiso -r -v -w .work -o out tealinux
+time sudo systemd-inhibit mkarchiso -r -v -w .work -o out tealinux
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+notify-send -u critical "Tealinux Build" "Exited. done in $elapsed_time seconds"
+mpv notif.mp3 > /dev/null 2>&1 &
--- build_with_installer.sh
+++ build_with_installer.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 sudo sed -i "s|Server = file://.*|Server = file://$(pwd)/localrepo/|" ./tealinux/pacman.conf
 
+start_time=$(date +%s)
+
 # Build and add installer to localrepo
 cd ./localrepo
 makepkg -fs
 repo-add localrepo.db.tar.xz tealinux-installer-git-2.0-1-x86_64.pkg.tar.zst
 cd ..
 
+# Build ISO
 sudo rm -rf .work
-sudo mkarchiso -r -v -w .work -o out tealinux
+time sudo systemd-inhibit mkarchiso -r -v -w .work -o out tealinux
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+notify-send -u critical "Tealinux Build" "Exited. done in $elapsed_time seconds"
+mpv notif.mp3 > /dev/null 2>&1 &
+
