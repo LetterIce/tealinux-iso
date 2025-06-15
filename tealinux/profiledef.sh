cc3b2413cb96c24c58e26e4ccebb1d80ab77b06c
gagahsyuja <gghssa1@gmail.com>
2025-06-16 02:00:25 +0700

    Improve iso naming

--- tealinux/profiledef.sh
+++ tealinux/profiledef.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="tealinux"
-iso_label="TEALINUX_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Tea Linux <https://tealinux.org>"
-iso_application="Tea Linux baseline"
+iso_edition="base"
+# iso_edition="cosmic"
+# iso_edition="plasma"
+iso_name="tealinux-celia-${iso_edition}"
+iso_label="TEALINUX-CELIA_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m)"
+iso_publisher="Tea Linux <https://tealinuxos.org>"
+iso_application="Tea Linux Celia"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
@@ -16,8 +19,15 @@ pacman_conf="pacman.conf"
 # airootfs_image_type="erofs"
 airootfs_image_type="squashfs"
 # airootfs_image_tool_options=('-zlzma,109' -E 'ztailpacking,fragments,dedupe')
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=(zstd -c -T0 --long -19)
+
+# Faster compression
+airootfs_image_tool_options=('-comp' 'zstd' '-b' '256K' '-Xcompression-level' '19')
+bootstrap_tarball_compression=(zstd -c -T0 --long -15)
+
+## Fall back if above fails
+# airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+# bootstrap_tarball_compression=(zstd -c -T0 --long -19)
+
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
