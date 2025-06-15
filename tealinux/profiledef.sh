1c72104750344ea31f487d44fe9c6f48e9b96746
gagahsyuja <gghssa1@gmail.com>
2025-06-16 03:40:04 +0700

    Fix installer desktop permission

--- tealinux/profiledef.sh
+++ tealinux/profiledef.sh
@@ -31,4 +31,5 @@ bootstrap_tarball_compression=(zstd -c -T0 --long -15)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
+  ["/etc/skel/Desktop/tealinux-installer-git.desktop"]="0:0:755"
 )
